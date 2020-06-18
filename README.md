598a905cb2dc846ce6b4955eaf32608549544802
chunman2008 <66762518+chunman2008@users.noreply.github.com>
2020-06-18 16:15:26 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Area
+# tools
+
+buliding my extension 
+
+## License
+
+ry 
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+Area=github:chunman2008/Area
+```
---
+++ main.ts
@@ -0,0 +1,30 @@
+//% weight=0 color=#b3a23c icon="\uf780" block="Area"
+namespace Area {
+    /**
+    * calculate the area of rectangle and return 
+    */
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1 blockExternalInputs=true 
+	/*   blockexternal = more than two blocks 
+	*/
+    export function areaOfRectangle(length: number, width:number): number {
+        return length*width
+    }
+    /**
+    *  calculate the area of rectangle and show it 
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=5 weight=0
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(areaOfRectangle(length*width))
+    }
+	
+	
+	export function LEDofTriangles(base:number,heigh:number):void{
+		basic.showNumber(0.5*base*heigh) 
+		
+		
+	}
+	
+	//% block 
+}
